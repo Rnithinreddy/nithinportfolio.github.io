01a3397d926eb73589d2cc283dc3c3e0e486962d
Rnithinreddy <152314109+Rnithinreddy@users.noreply.github.com>
2024-02-14 20:12:58 +0530

    Add files via upload

---
+++ vscode/js/script.js
@@ -0,0 +1,7 @@
+/* ====================== typing animation ======================= */
+var typed = new typed(".typing", {
+    Strings:["", "full-stack developer"],
+    typespeed:100,
+    backspeed:60,
+    loop:true
+})
